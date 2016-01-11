d7ab6a5f2148a6c972d020e4ba1132c1ce3e6f52
Ben Harrison <ben@benthedeveloper.com>
2016-01-11 11:35:39 -0800

    Added a few test logs for getting movies using discover

--- app/src/main/java/com/benthedeveloper/popmovies/MainActivityFragment.java
+++ app/src/main/java/com/benthedeveloper/popmovies/MainActivityFragment.java
@@ -1,7 +1,8 @@
 package com.benthedeveloper.popmovies;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +12,25 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // Test getting some data using theMovieDB
+        String baseURL = "http://api.themoviedb.org/3/";
+        String discoverMethodURL = "discover/movie";
+        String mostPopularParam = "?sort_by=popularity.desc";
+        String highestRatedParam = "?sort_by=vote_average.desc";
+        String apiKeyParam = "&api_key=" + BuildConfig.THE_MOVIE_DB_API_TOKEN;
+        Log.v(LOG_TAG, "most popular URL: " + baseURL + discoverMethodURL + mostPopularParam + apiKeyParam);
+        Log.v(LOG_TAG, "highest rated URL: " + baseURL + discoverMethodURL + highestRatedParam + apiKeyParam);
+        // End test getting some data using theMovieDB
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
