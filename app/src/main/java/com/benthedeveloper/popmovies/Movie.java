5f9d7cacc5fac1fc7941c4dc46781219a5a03594
Ben Harrison <ben@benthedeveloper.com>
2016-01-22 13:12:28 -0800

    Used parcelabler.com to implement Parcelable interface on Movie class

--- app/src/main/java/com/benthedeveloper/popmovies/Movie.java
+++ app/src/main/java/com/benthedeveloper/popmovies/Movie.java
@@ -1,10 +1,13 @@
 package com.benthedeveloper.popmovies;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Ben Harrison on 1/19/2016.
  * Object to represent a movie for the app
  */
-public class Movie {
+public class Movie implements Parcelable {
     private int id;
     private String originalTitle;
     private String posterURL;
@@ -23,6 +26,7 @@ public class Movie {
     }
 
     // toString method
+    @Override
     public String toString() {
         return "id: " + getId() + "\n"
                 + "originalTitle: " + getOriginalTitle() + "\n"
@@ -57,4 +61,42 @@ public class Movie {
         }
         return this.releaseDateStr;
     }
+
+
+    protected Movie(Parcel in) {
+        id = in.readInt();
+        originalTitle = in.readString();
+        posterURL = in.readString();
+        overview = in.readString();
+        voteAverage = in.readDouble();
+        releaseDateStr = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(originalTitle);
+        dest.writeString(posterURL);
+        dest.writeString(overview);
+        dest.writeDouble(voteAverage);
+        dest.writeString(releaseDateStr);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 }
